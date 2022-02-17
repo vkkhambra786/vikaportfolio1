b21c396966eb1ff7ddd1e9c316f20b67c7b18e95
vkkhambra786 <vkkhambra786@gmail.com>
2022-02-17 12:17:25 +0530

    Changes potfolio 17 feb 2nd time

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,13 +12,13 @@ export default function About() {
         </div>
         <div>
           <p>
-            I am a Full-stack web developer from India. Self motivated
-            and curious with a keen interest in building user friendly products.
-            My field of interests are building new web technologies, products
-            and also in areas related to deep learning. I love to create
-            beautiful and efficient websites for users. I keenly apply my
-            knowledge for developing products with node.js, modern Javascript
-            Library, mongoDB, express and frameworks like react.js
+            I am a Full-stack web developer from India. Self motivated and
+            curious with a keen interest in building user friendly products. My
+            field of interests are building new web technologies, products and
+            also in areas related to deep learning. I love to create beautiful
+            and efficient websites for users. I keenly apply my knowledge for
+            developing products with node.js, modern Javascript Library,
+            mongoDB, express and frameworks like react.js
           </p>
         </div>
       </div>
