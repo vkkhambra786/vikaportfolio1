b0bc880f3307384c7e419662c642b1cede8e5231
vkkhambra786 <vkkhambra786@gmail.com>
2022-02-17 12:16:01 +0530

    Changes potfolio 17 feb

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
         </div>
         <div>
           <p>
-            I am a Vikas a Full-stack web developer from India. Self motivated
+            I am a Full-stack web developer from India. Self motivated
             and curious with a keen interest in building user friendly products.
             My field of interests are building new web technologies, products
             and also in areas related to deep learning. I love to create
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="name">
         <h1>Hi there!</h1>
         <h1>I'm Vikas Khambra</h1>
-        <h1>A Full-Stack Web Developer</h1>
+        <h1>Full-Stack Web Developer</h1>
         <a
           href="https://drive.google.com/file/d/1g3J8FE_R4ByEY3QoctBY3moH_eljooc8/view?usp=sharing"
           target="_blank"
