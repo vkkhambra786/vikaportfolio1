17c64d201d40e4af2af2338811838e87157aeba8
vkkhambra786 <vkkhambra786@gmail.com>
2022-06-27 18:52:23 +0530

    push the react code

--- src/components/Proj/Proj.jsx
+++ src/components/Proj/Proj.jsx
@@ -1,6 +1,8 @@
 import img1 from "./pulse.PNG";
-import img2 from "./health.PNG";
+import img2 from "./tictacttoe.PNG";
 import img3 from "./tanshiq.PNG";
+import img4 from "./todopep1.PNG";
+import img5 from "./todoo.PNG";
 import React from "react";
 import("./Proj.css");
 export default function Proj(props) {
@@ -13,11 +15,11 @@ export default function Proj(props) {
             <img src={img2} alt="" />
           </div>
           <div>
-            <h2>Bemz clone</h2>
+            <h2> TIC TAC TOE GAME</h2>
             <p>
-              Bemz.com is India's largest online IKEA furniture store for home
-              and office. Shop online from the latest collections of bed, sofa
-              and similar products featuring the best brands.
+              Tic Tac Toe Game is play by two person build on ReactJs. You click
+              one box at one time , you can go o your previous step and it show
+              win , lose and draw
             </p>
             <h3>Tech Stack</h3>
 
@@ -34,17 +36,120 @@ export default function Proj(props) {
                 <i class="fab fa-js"></i>
                 <p>JavaScript</p>
               </div>
+              <div>
+                <i class="fab fa-react"></i>
+                <p>ReactJs</p>
+              </div>
             </div>
             <div className="buttons">
               <a
-                href="https://bemz-1.vercel.app/"
+                href="https://tic-tac-toe-pepcoding2.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
                 <button>Preview</button>
               </a>
               <a
-                href="https://github.com/vkkhambra786/BEMZ-1.git"
+                href="https://github.com/vkkhambra786/TIC_TAC_TOE_.git"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button>Code</button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="final">
+          <div className="img">
+            <img src={img4} alt="" />
+          </div>
+          <div>
+            <h2> TODO LIST</h2>
+            <p>
+              Todo List App you can add the data use input box. If you click on
+              tick mark then it add to finished task if you click on cross marks
+              it ad on Todo task.
+            </p>
+            <h3>Tech Stack</h3>
+
+            <div className="tech">
+              <div>
+                <i class="fab fa-html5"></i>
+                <p>HTML</p>
+              </div>
+              <div>
+                <i class="fab fa-css3"></i>
+                <p>CSS</p>
+              </div>
+              <div>
+                <i class="fab fa-js"></i>
+                <p>JavaScript</p>
+              </div>
+              <div>
+                <i class="fab fa-react"></i>
+                <p>ReactJs</p>
+              </div>
+            </div>
+            <div className="buttons">
+              <a
+                href="https://todoapp-pepcoding1.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button>Preview</button>
+              </a>
+              <a
+                href="https://github.com/vkkhambra786/TodoappPepcoding1.git"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button>Code</button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="final">
+          <div className="img">
+            <img src={img5} alt="" />
+          </div>
+          <div>
+            <h2> TODO LIST </h2>
+            <p>
+              Todo List you can add the data my using all input box craete by
+              ReactJs.
+            </p>
+            <h3>Tech Stack</h3>
+
+            <div className="tech">
+              <div>
+                <i class="fab fa-html5"></i>
+                <p>HTML</p>
+              </div>
+              <div>
+                <i class="fab fa-css3"></i>
+                <p>CSS</p>
+              </div>
+              <div>
+                <i class="fab fa-js"></i>
+                <p>JavaScript</p>
+              </div>
+              <div>
+                <i class="fab fa-react"></i>
+                <p>ReactJs</p>
+              </div>
+            </div>
+            <div className="buttons">
+              <a
+                href="https://todo-list-react-three-gamma.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button>Preview</button>
+              </a>
+              <a
+                href="https://github.com/vkkhambra786/TODO_LIST_React.git"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -100,7 +205,6 @@ export default function Proj(props) {
             </div>
           </div>
         </div>
-
         <div className="final">
           <div className="img">
             <img src={img3} alt="" />
